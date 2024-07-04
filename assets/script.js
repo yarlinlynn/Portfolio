db57f1d7e0b4f7f20182459fb6568741adfc18ad
Yarlin Lynn <140059481+yarlinlynn@users.noreply.github.com>
2024-07-04 15:16:56 +0200

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+const $tabBtn = document.querySelectorAll("[data-tab-btn]");
+let [lastActiveTab] = document.querySelectorAll("[data-tab-content]");
+let [lastActiveTabBtn] = $tabBtn;
+
+$tabBtn.forEach(item =>{
+    item.addEventListener("click", function () {
+        lastActiveTab.classList.remove("active");
+        lastActiveTabBtn.classList.remove("active");
+
+        const $tabContent = document.querySelector(`[data-tab-content="${item.dataset.tabBtn}"]`);
+        $tabContent.classList.add("active");
+        this.classList.add("active");
+
+        lastActiveTab = $tabContent;
+        lastActiveTabBtn = this
+    });
+});
