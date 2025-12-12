c8291f7c6bb23f338d29a5108c0add3040d1c5aa
Yarlin <yarlinlynn@gmail.com>
2025-12-12 17:34:55 +0200

    Created and styled the popup window that appears when project first loads and add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 
-import Portfolio from "./PortfolioPage.jsx";
+// import Portfolio from "./PortfolioPage.jsx";
+import PopupWindow from "./components/PopupWindow.jsx";
 
 import './App.css'
 
 function App() {
 
 
-  return <Portfolio />
+  return (
+    <>
+      <PopupWindow/>
+    </>
+  )
 
 }
 
---
+++ src/components/PopupWindow.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+
+import icon from '../assets/images/msg_question-0.png'
+
+function PopupWindow() {
+
+  return (
+
+        <div className="popup transtion">
+            <div className='window'>
+                <div className='title-bar'>
+                    <div className='title-bar-text'>System message</div>
+                    <div className="title-bar-controls">
+                        <button aria-label="Help" disabled></button>
+                        <button aria-label="Close"></button>
+                    </div>
+                </div>
+                <div className="window-body">
+                    <p>
+                        <span className='window-icon'>
+                            <img src={icon} alt="popup window icon"/>
+                        </span>
+                        Welcome! Before you can explore my portfolio, please choose an option below to fit what you're looking for.
+                    </p>
+
+                    <button>I need a website</button>
+                    <button>I need a developer</button>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default PopupWindow
