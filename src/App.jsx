fbac317943aeef57ea9dba822661658532a95ca2
Yarlin <yarlinlynn@gmail.com>
2025-12-15 16:44:50 +0200

    Created and styled a loading window animation before PortfolioPage component loads

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-
 
 import { usePortfolio } from "./context/PortfolioContext.jsx";
+
 import PopupWindow from "./components/PopupWindow.jsx";
+import LoadingWindow from './components/LoadingWindow.jsx';
 import Portfolio from "./PortfolioPage.jsx";
 
 import './App.css'
@@ -12,8 +11,33 @@ import './App.css'
 function App() {
 
   const { version } = usePortfolio();
+  const [loading, setLoading] = useState(false);
+
+  // useEffect( () => {
+  //   // if(version) {
+  //   //   setLoading(true);
+  //   //       setTimeout(() => setLoading(false), 5000);
+  //   // }
+  //   if (version) {
+  //     setLoading(true);
+
+  //     // simulate "app boot"
+  //     const timer = setTimeout(() => {
+  //       setLoading(false);
+  //     }, 4500);
+
+  //     return () => clearTimeout(timer);
+  //   }
+  // }, version)
+  
+  useEffect(() => {
+    if (version) {
+      setLoading(true);
+    }
+  }, [version]);
 
   if (!version) return <PopupWindow/>
+  if (loading) return <LoadingWindow onComplete={() => setLoading(false)} />
 
   return <Portfolio/>
 
